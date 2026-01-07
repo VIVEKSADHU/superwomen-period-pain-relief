8578c27e03315dad66a37740029369d6dade0e29
Vivek <viveksadhu369@gmail.com>
2026-01-07 07:09:08 +0000

    Try fixing this error: `Build Error: Export nextPlugin doesn't exist in

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,16 +1,9 @@
 import {genkit, Plugin} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import {nextPlugin} from '@genkit-ai/next';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    nextPlugin({
-      // We are mounting the Genkit API on `/api/genkit`.
-      // By default, Next.js has a 30-second timeout on API routes.
-      // We can increase this to accommodate longer-running flows.
-      // maxDuration: 60,
-    }),
   ],
   // model: 'googleai/gemini-2.5-flash',
 });
